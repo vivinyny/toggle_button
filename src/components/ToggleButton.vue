7c2fe34403254ab8cda9123e18e8cdf94e814647
Vilouny Phoubandith <vilouny.ph@gmail.com>
2020-11-25 17:32:05 +0100

    package + template toggle button

---
+++ src/components/ToggleButton.vue
@@ -0,0 +1,21 @@
+<template>
+    <label for="toggle-button">
+        <span>On</span>
+        <span>Off</span>
+
+        <input type="checkbox" id="toggle_button">
+    </label>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            currentState: false
+        }
+    }
+}
+</script>
+
+<style>
+</style>
