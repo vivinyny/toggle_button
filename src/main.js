cfccefcd2a18a3a4febdafdfd9385cea6348eb87
Vilouny Phoubandith <vilouny.ph@gmail.com>
2020-11-26 11:55:04 +0100

    import router mainjs

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import { createApp } from 'vue';
 
+import router from '/router.js';
 import App from './App.vue';
 import ToggleButton from './components/ToggleButton.vue'
 
 const app = createApp(App)
+
+app.use(store);
 
 app.component('toggle-button', ToggleButton);
 
